145b4b36d954fcfcc23e4016fc509cdcd7a4b7ca
Kozma János <67119975+jkozma78@users.noreply.github.com>
2021-07-17 13:26:55 +0200

    Add files via upload

---
+++ pages/test_ddgo.py
@@ -0,0 +1,28 @@
+import time
+
+import pytest
+from selenium.webdriver import Chrome
+from pages.search import DuckDuckGoSearchPage
+from pages.results import DuckDuckGoResultPage
+
+
+@pytest.fixture
+def browser():
+    driver = Chrome()
+    driver.implicitly_wait(10)
+    return driver
+
+
+def test_basic_duckduckgo_search(browser):
+    PHRASE = 'panda'
+    search_page = DuckDuckGoSearchPage(browser)
+    search_page.load()
+    time.sleep(3)
+
+    search_page.search(PHRASE)
+    time.sleep(3)
+    result_page = DuckDuckGoResultPage(browser)
+
+    assert result_page.link_div_count() > 0
+    assert result_page.phrase_result_count(PHRASE) > 0
+    assert result_page.search_input_value() == PHRASE
